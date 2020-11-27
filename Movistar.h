e7392397b88a29c5e71e0c0b94b9fc28d0470abf
LeilaelRico <74067307+LeilaelRico@users.noreply.github.com>
2020-11-26 21:45:18 -0600

    Add files via upload

---
+++ Movistar.h
@@ -0,0 +1,14 @@
+
+
+#include "creProveedor.h"
+
+
+class Movistar:public Proveedor{
+
+public:
+    Movistar(string com, string cos, string dur, string meg):Proveedor(com, cos, dur, meg){
+
+    }
+
+};
+
---
+++ Telcel.h
@@ -0,0 +1,10 @@
+
+#include "creProveedor.h"
+
+class Telcel:public Proveedor{
+
+public:
+    Telcel(string com, string cos, string dur, string meg):Proveedor(com, cos, dur, meg){
+    }
+
+};
